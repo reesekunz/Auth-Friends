d69a02df9f99715ea67167000a115c425cbeddbd
Reese Kunz <reesekunz21@gmail.com>
2019-08-20 14:38:22 -0700

    built out private route

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,52 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Form, Field, withFormik } from 'formik';
-import * as Yup from 'yup';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Form, Field, withFormik } from "formik";
+import * as Yup from "yup";
 
 const LoginForm = ({ errors, touched, values, handleSubmit, status }) => {
-    
-    return (
-      <div className="login-form">
-        <h1>Login</h1>
-        <Form>
-          <Field type="text" name="username" placeholder="username" />*
-          {touched.username && errors.username && (
-            <p className="error">{errors.username}</p>
-          )}
-  
-          <Field type="password" name="password" placeholder="password" />*
-          {touched.size && errors.size && <p className="error">{errors.size}</p>}
+  return (
+    <div className="login-form">
+      <h1>Login</h1>
+      <Form>
+        <Field type="text" name="username" placeholder="username" />*
+        {touched.username && errors.username && (
+          <p className="error">{errors.username}</p>
+        )}
+        <Field type="password" name="password" placeholder="password" />*
+        {touched.size && errors.size && <p className="error">{errors.size}</p>}
+        <button type="submit">Submit!</button>
+      </Form>
+    </div>
+  );
+};
 
-  
-          <button type="submit">Submit!</button>
-        </Form>
-          </div>
-       
-    );
-  };
+const FormikLoginForm = withFormik({
+  mapPropsToValues({ username, password }) {
+    return {
+      username: username || "",
+      password: password || ""
+    };
+  },
 
-  const FormikLoginForm = withFormik({
-    mapPropsToValues({ username, password }) {
-      return {
-       
-        username: username || '',
-        password: password || ''
-      };
-    },
-  
-    validationSchema: Yup.object().shape({
-      username: Yup.string().required(),
-      password: Yup.string().required(),
-    }),
-  
-    handleSubmit(values, { setStatus }) {
-        axios
-          .post('http://localhost:5000/api/login', values)
-          .then(response => console.log(response))
-          .catch(error => console.log(error.response));
-      }
-    })(LoginForm); // currying functions in Javascript
-    
-    export default FormikLoginForm;
-    
-      
+  validationSchema: Yup.object().shape({
+    username: Yup.string().required(),
+    password: Yup.string().required()
+  }),
+
+  handleSubmit(values, { setStatus }) {
+    axios
+      .post("http://localhost:5000/api/login", values)
+      .then(response => {
+        localStorage.setItem("token", response.data.payload);
+        // localStorage.setItem("token", JSON.stringify(response.data) - if want to send entire array or object to local storage
+      })
+      .catch(error => console.log(error.response));
+  }
+})(LoginForm);
+
+export default FormikLoginForm;
+
+// Login Credentials = username: Lambda School, password: i<3Lambd4
+// .then console.log(response) = response.data.payload
--- friends/src/components/PrivateRoute.js
+++ friends/src/components/PrivateRoute.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router';
+import React from "react";
+import { Route, Redirect } from "react-router";
 
-// requirements: 
+// requirements:
 // 1. It has the same API as `<Route />` - Have the same props
 // 2. It renders a <Route /> and passes all the props through to it.
 
+// This code for Private Route Component can be reused in every project:
 const PrivateRoute = ({ component: Component, ...rest }) => {
   return (
     <Route
       {...rest}
       render={props => {
-        if (localStorage.getItem('token')) {
+        if (localStorage.getItem("token")) {
           return <Component {...props} />;
         }
         return <Redirect to="/login" />;
@@ -21,7 +22,8 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
 
 export default PrivateRoute;
 
-// ({ component, ...rest }) is shorthand for  👇
+// ({ component, ...rest }) is shorthand for:
+
 // const props = {
 //   component: {},
 //   exact: true,
